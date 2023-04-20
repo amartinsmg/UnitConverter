12d2fa14c3e0a69c4c195fd582805bccbfbd1e23
andre <andremindurimg@gmail.com>
2023-04-20 20:03:41 -0300

    Update test

--- src/test/main.js
+++ src/test/main.js
@@ -1,5 +1,25 @@
 const { ok: assert } = require("assert"),
-  { readFileSync } = require("fs");
+  {
+    lengthConversion,
+    areaConversion,
+    volumeConversion,
+    massConversion,
+    pressureConversion,
+    timeConversion,
+    speedConversion,
+    accelerationConversion,
+    forceConversion,
+    temperatureConversion,
+    energyConversion,
+    powerConversion,
+  } = require("../converter");
+
+/**
+  Rounds a number to a specified number of decimal places
+  @param num - The number to be rounded
+  @param decimalPlaces - The number of decimal places to round to
+  @return - The rounded number
+*/
 
 function roundTo(num, decimalPlaces) {
   let base10 = 10 ** decimalPlaces,
@@ -7,39 +27,22 @@ function roundTo(num, decimalPlaces) {
   return result;
 }
 
-void (async function () {
-  const WasmBuffer = readFileSync(__dirname + "/../../build/assets/main.wasm"),
-    WasmModule = await WebAssembly.instantiate(WasmBuffer),
-    {
-      length,
-      area,
-      volume,
-      mass,
-      pressure,
-      time,
-      speed,
-      acceleration,
-      force,
-      temperature,
-      energy,
-      power,
-    } = WasmModule.instance.exports;
-
-  assert(roundTo(length(8, 6, 1), 3) == 1.852);
-  assert(roundTo(length(5, 4, 1), 3) == 12);
-  assert(roundTo(area(7, 5, 1), 3) == 258.999);
-  assert(roundTo(area(3, 2, 1), 3) == 6.452);
-  assert(roundTo(volume(6, 5, 1), 3) == 3.785);
-  assert(roundTo(volume(4, 2, 10), 3) == 295.735);
-  assert(roundTo(mass(6, 5, 1), 3) == 2204.623);
-  assert(roundTo(mass(4, 3, 1), 3) == 28.35);
-  assert(roundTo(pressure(5, 4, 1), 3) == 1.013);
-  assert(roundTo(time(5, 3, 1), 3) == 8766);
-  assert(roundTo(speed(4, 3, 1), 3) == 1.609);
-  assert(roundTo(acceleration(6, 3, 1), 3) == 35.304);
-  assert(roundTo(force(4, 2, 1), 3) == 980665);
-  assert(roundTo(temperature(2, 3, 20), 3) == 68);
-  assert(roundTo(energy(4, 2, 1), 3) == 3600);
-  assert(roundTo(power(4, 2, 1), 3) == 2684.52);
+void (function () {
+  assert(roundTo(lengthConversion(8, 6, 1), 3) == 1.852);
+  assert(roundTo(lengthConversion(5, 4, 1), 3) == 12);
+  assert(roundTo(areaConversion(7, 5, 1), 3) == 258.999);
+  assert(roundTo(areaConversion(3, 2, 1), 3) == 6.452);
+  assert(roundTo(volumeConversion(6, 5, 1), 3) == 3.785);
+  assert(roundTo(volumeConversion(4, 2, 10), 3) == 295.735);
+  assert(roundTo(massConversion(6, 5, 1), 3) == 2204.623);
+  assert(roundTo(massConversion(4, 3, 1), 3) == 28.35);
+  assert(roundTo(pressureConversion(5, 4, 1), 3) == 1.013);
+  assert(roundTo(timeConversion(5, 3, 1), 3) == 8766);
+  assert(roundTo(speedConversion(4, 3, 1), 3) == 1.609);
+  assert(roundTo(accelerationConversion(6, 3, 1), 3) == 35.304);
+  assert(roundTo(forceConversion(4, 2, 1), 3) == 980665);
+  assert(roundTo(temperatureConversion(2, 3, 20), 3) == 68);
+  assert(roundTo(energyConversion(4, 2, 1), 3) == 3600);
+  assert(roundTo(powerConversion(4, 2, 1), 3) == 2684.52);
   console.log("Passed all tests successfully");
 })();
