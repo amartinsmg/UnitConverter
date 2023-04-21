7c354f492854520d61958aca53294467ada28787
andre <andremindurimg@gmail.com>
2023-04-21 01:29:58 -0300

    Update test

--- src/test/main.js
+++ src/test/main.js
@@ -16,16 +16,22 @@ const { ok: assert } = require("assert"),
 
 /**
   Rounds a number to a specified number of decimal places
-  @param num - The number to be rounded
-  @param decimalPlaces - The number of decimal places to round to
-  @return - The rounded number
-*/
+    @param num - The number to be rounded
+    @param decimalPlaces - The number of decimal places to round to
+    @return - The rounded number
+ */
 
 function roundTo(num, decimalPlaces) {
   let base10 = 10 ** decimalPlaces,
     result = Math.round(num * base10) / base10;
   return result;
 }
+
+/**
+  This function runs some tests to assert that the functions produce expected results.
+  The tests pass if all sixteen assertions succeed. If any of the assertions fail, an error will be thrown.
+  Finally, a success message is logged to the console.
+ */
 
 void (function () {
   assert(roundTo(lengthConversion(8, 6, 1), 3) == 1.852);
